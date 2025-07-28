8cbe8073c29ad560fcf964cc8e50d086d1110620
ritikkr70 <ritikkr70@gmail.com>
2025-07-28 10:40:29 +0530

    Fix

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -32,7 +32,7 @@ export const DashboardUserButton = () => {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bgwhite/10 overflow-hidden">
+      <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden">
         {data.user.image ? (
           <Avatar className="size-9 mr-3">
             <AvatarImage src={data.user.image} />
