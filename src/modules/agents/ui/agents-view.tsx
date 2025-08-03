98124e4262cdf50daaef6624afc242fb83943675
ritikkr70 <ritikkr70@gmail.com>
2025-08-04 00:50:23 +0530

    Pagination and Search

--- src/modules/agents/ui/agents-view.tsx
+++ src/modules/agents/ui/agents-view.tsx
@@ -6,17 +6,30 @@ import { columns } from "./components/columns";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { EmptyState } from "@/components/empty-state";
+import { useAgentsFilters } from "../hooks/use-agents-filters";
+import { Datapagination } from "./components/data-pagination";
 
 export const AgentsView = () => {
+  const [filters,setFilters] = useAgentsFilters();
   const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+  const { data } = useSuspenseQuery(
+    trpc.agents.getMany.queryOptions({ ...filters })
+  );
 
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-      <DataTable data={data} columns={columns} />
-      {data.length === 0 && (
-        <EmptyState title="Create Your First Agent" description="Create an agent to join your meetings. Each agent will follow
-        your instructions and can interact with participants during the call"/>
+      <DataTable data={data.items} columns={columns} />
+      <Datapagination
+      page={filters.page}
+      totalPages={data.totalPages}
+      onPageChange={(page) => setFilters({  page })}
+      />
+      {data.items.length === 0 && (
+        <EmptyState
+          title="Create Your First Agent"
+          description="Create an agent to join your meetings. Each agent will follow
+        your instructions and can interact with participants during the call"
+        />
       )}
     </div>
   );
--- src/modules/agents/ui/components/columns.tsx
+++ src/modules/agents/ui/components/columns.tsx
@@ -32,9 +32,11 @@ export const columns: ColumnDef<AgentGetOne>[] = [
   {
     accessorKey: "meetingCount",
     header: "Meetings",
-    cell: () => (
+    cell: ({ row }) => (
       <Badge variant="outline" className="flex items-center gap-x-2 ">
-        <VideoIcon className="text-blue-700" />5 meetings
+        <VideoIcon className="text-blue-700" />
+        {row.original.meetingCount}{" "}
+        {row.original.meetingCount === 1 ? "meeting" : "meetings"}
       </Badge>
     ),
   },
