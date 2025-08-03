41e89f0a3b9fa2bd6c6f8b8362b55b25bcea2b9c
ritikkr70 <ritikkr70@gmail.com>
2025-08-03 16:44:57 +0530

    Empty State

--- src/modules/agents/ui/agents-view.tsx
+++ src/modules/agents/ui/agents-view.tsx
@@ -1,29 +1,41 @@
-"use client"
+"use client";
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
+import { DataTable } from "./components/data-table";
+import { columns } from "./components/columns";
 import { useTRPC } from "@/trpc/client";
-import {  useSuspenseQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { EmptyState } from "@/components/empty-state";
 
 export const AgentsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-  
   return (
-  <div>
-    {JSON.stringify(data,null)}
-  </div>
-    );
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable data={data} columns={columns} />
+      {data.length === 0 && (
+        <EmptyState title="Create Your First Agent" description="Create an agent to join your meetings. Each agent will follow
+        your instructions and can interact with participants during the call"/>
+      )}
+    </div>
+  );
 };
 
-export const AgentsViewLoading=()=>{
-    return(
-        <LoadingState title="Loading Agents" description="This may take a few seconds"/>
-    )
-}
+export const AgentsViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading Agents"
+      description="This may take a few seconds"
+    />
+  );
+};
 
-export const AgentsViewError=()=>{
-    return(
-        <ErrorState title="Error Loading Agents" description="Please try again later"/>
-    )
-}
+export const AgentsViewError = () => {
+  return (
+    <ErrorState
+      title="Error Loading Agents"
+      description="Please try again later"
+    />
+  );
+};
