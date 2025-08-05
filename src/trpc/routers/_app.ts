784dc9fa5bdbd3efb646bf81c6cd985229415741
ritikkr70 <ritikkr70@gmail.com>
2025-08-06 00:20:28 +0530

    Setup

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,11 @@
-
 import { agentsRouter } from "@/modules/agents/server/procedures";
 
 import { createTRPCRouter } from "../init";
-
+import { meetingsRouter } from "@/modules/meetings/server/procedures";
 
 export const appRouter = createTRPCRouter({
   agents: agentsRouter,
+  meetings: meetingsRouter,
 });
 
 export type AppRouter = typeof appRouter;
