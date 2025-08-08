5a3bc28d9cd546ba827d802fd79cd5f20c5f3a1a
ritikkr70 <ritikkr70@gmail.com>
2025-08-08 18:24:24 +0530

    Fixes

--- src/modules/meetings/ui/components/meetings-list-header.tsx
+++ src/modules/meetings/ui/components/meetings-list-header.tsx
@@ -4,9 +4,11 @@ import { PlusIcon, XCircleIcon } from "lucide-react";
 import { NewMeetingDialog } from "./new-meeting-dialog";
 import { useState } from "react";
 import { MeetingsSearchFilters } from "./meetings-search-filter";
-import { StatusFilter} from "./status-filter";
+import { StatusFilter } from "./status-filter";
 import { AgentIdFilter } from "./agent-id-filter";
 import { useMeetingsFilters } from "../../hooks/use-meetings-filters";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { DEFAULT_PAGE } from "@/constants";
 
 export const MeetingsListHeader = () => {
   const [filters, setFilters] = useMeetingsFilters();
@@ -20,13 +22,13 @@ export const MeetingsListHeader = () => {
       status: null,
       agentId: "",
       search: "",
-      page: 1,
+      page: DEFAULT_PAGE,
     });
   };
   return (
     <>
       <NewMeetingDialog open={isDialogOpen} onOpenChange={setDialogOpen} />
-      <div className="py-4 md:px-8 flex flex-col gap-y-4">
+      <div className="py-4 px-4 md:px-8 flex flex-col gap-y-4">
         <div className="flex items-center justify-between">
           <h5 className="font-medium text-xl">My Meetings</h5>
           <Button onClick={() => setDialogOpen(true)}>
@@ -34,17 +36,20 @@ export const MeetingsListHeader = () => {
             New Meeting
           </Button>
         </div>
-        <div className="flex items-center gap-x-2 p-1">
-          <MeetingsSearchFilters />
-          <StatusFilter />
-          <AgentIdFilter />
-          {isAnyFilterModified && (
-            <Button variant="ghost" onClick={onClearFilters}>
-              <XCircleIcon />
-              Clear
-            </Button>
-          )}
-        </div>
+        <ScrollArea>
+          <div className="flex items-center gap-x-2 p-1">
+            <MeetingsSearchFilters />
+            <StatusFilter />
+            <AgentIdFilter />
+            {isAnyFilterModified && (
+              <Button variant="ghost" onClick={onClearFilters}>
+                <XCircleIcon />
+                Clear
+              </Button>
+            )}
+          </div>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
       </div>
     </>
   );
