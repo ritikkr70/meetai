e42ac1dedda273c17f1e5634ee63708b42f0eb4e
ritikkr70 <ritikkr70@gmail.com>
2025-08-15 23:34:23 +0530

    Fix

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -5,5 +5,5 @@ import { serve } from "inngest/next";
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [meetingsProcessing],
+  functions: [meetingsProcessing]
 });
